e4788ae9c770a66b24e09496d840d770e7ebfd7f
Roland Jablonski <rolandjablonski@Rolands-MacBook-Air.local>
2017-03-30 14:46:03 +0100

    zmiana java

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@
 var name = prompt('Roland');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+/*
+  alert ('Witaj');
+  console.log(‘Zakomentowana się nie wywołam’);
+*/
+// komentarz liniowy
+
+// poniżej zakomentowałem wywołanie funkcji, aby ją wyłączyć
+// console.log(‘Zakomentowana się nie wywoła’);
